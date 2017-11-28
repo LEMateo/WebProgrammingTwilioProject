13a151713e7a29dfe8c092214fc4cd8b365ca9e4
Lauren Mateo <stlem04@moravian.edu>
2017-11-28 17:16:42 -0500

    added changes to get

--- Alert_App.py
+++ Alert_App.py
@@ -1,10 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/get')
 def get():
+    #error check
+    results=[]
+
+    #add first 10 reminders in database to results
 
 
+    #return results
+
+@app.route('/set')
 def set():
 
-
+@app.route('/drop')
 def drop():
