ec0bf92a667c694c255d3bf712165f616932ac96
Lauren Mateo <stlem04@moravian.edu>
2017-12-01 11:35:28 -0500

    Added more comments about the structure, added a delete

--- Alert_App.py
+++ Alert_App.py
@@ -1,11 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+from Alert_Database import Alert_Database
+ad=Alert_Database({})
 
 @app.route('/get')
 def get():
     #error check
     results=[]
-
+    #dont know if it works!!
+    results=ad.get_reminders()
     #add first 10 reminders in database to results
 
 
@@ -20,7 +23,8 @@ def get():
     #nothing to return
 
 
-#@app.route('/drop')
-#def drop(numID):
+@app.route('/drop')
+def drop(numID):
     #identify the reminder by the number id it was given aka numID
+    ad.delete_reminder(numID)
 
