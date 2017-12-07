812de921098349015a5c1320ee713a27c70e01fc
Lauren Mateo <stlem04@moravian.edu>
2017-12-07 15:54:20 -0500

    Added more tests

--- tests/Test_Alert_App.py
+++ tests/Test_Alert_App.py
@@ -97,10 +97,16 @@ class TestReminderDatabase(TestCase):
         db.new_reminder("2:00:00","R1")
         db.new_reminder("5:00:00","R2")
         db.delete_reminder(1)
-        #lmao this doesn't work yet btw
         self.assertEqual(1,len(db.get_reminders()))
 
-    # def test_remove_from_empty_database(self):
+    def test_remove_from_empty_database(self):
+        db=make_empty_database()
+        try:
+            db.delete_reminder(4)
+        except:
+            print("It can't delete lmao")
+        #self.assertEqual("I'm sorry. 1 is not a valid reminder code.",db.delete_reminder(4))
+
 
     # def test_remove_only_entry(self):
 
