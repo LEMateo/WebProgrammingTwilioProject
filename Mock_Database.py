bbe28dc994113589223a004d68adcc2bd52571f5
Andrew <stajc06@moravian.edu>
2017-12-07 15:08:32 -0500

    Added print() statement to inform user of newly added reminder's ID.

--- Mock_Database.py
+++ Mock_Database.py
@@ -33,16 +33,17 @@ class Mock_Database:
         # reminder_instance is the object.
         # Take the time and text and put it into one object, then pull the values when needed
         reminder_instance = Reminder_2.Reminder2(time, message)
-        #print(reminder_instance.get_id())
 
         # Adds the new reminder to the database.
         self.r.set(self.id_num, reminder_instance.reminder)
+        print("Reminder successfully added.  The reminder's ID is ", self.id_num, ".")
 
         # It might be a good idea to sort the reminders by time
         self.r.sort(self.r, by=time)
 
         # Increment id_num to prepare for next reminder
         self.id_num += 1
+
 
     def delete_reminder(self, numID):
         # use for drop
